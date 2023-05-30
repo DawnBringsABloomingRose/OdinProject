91e3da3432b6de61cd6fbac5dfb49aed3772417c
DawnBringsABloomingARose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-05-30 19:57:44 -0400

    Add a function to play a round of RPS

--- 02-rock-paper-scissors/script.js
+++ 02-rock-paper-scissors/script.js
@@ -1,3 +1,4 @@
+//a function to let the CPU choose rock paper or scissors randomly
 function getComputerChoice() {
     let computerChoice;
     let randomNumber = Math.floor(Math.random()*3);
@@ -15,4 +16,34 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+
+//a function to play a single round of Rock, paper, scissors
+function playRPS(playerSelection, computerSelection){
+    if (playerSelection == computerSelection) {
+        return `It was a tie! You both chose ${playerSelection}`
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats rock";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats paper";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats paper";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock";
+    }
+    return "error";
+
+}
+
+let playerSelection = prompt("Please enter rock, paper or scissors").toLowerCase();
+
+console.log(playRPS(playerSelection, getComputerChoice()));
