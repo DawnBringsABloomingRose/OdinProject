ba6bf3f7fef1d516de98ee45a33a05444a5d522a
DawnBringsABloomingARose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-05-31 18:15:46 -0400

    Add the ability to dynamically change the size of the etch-a-sketch grid

--- 03-etch-a-sketch/script.js
+++ 03-etch-a-sketch/script.js
@@ -1,18 +1,34 @@
 function toggleActive(item) {
-    console.log(item.target);
     item.target.classList.toggle('active');
 }
 
+function makeGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-element');
+        gridItem.style.width = `${100/size}%`
+        gridItem.style.paddingTop = `${100/size}%`
+        gridItem.addEventListener('mouseenter', toggleActive);
+    
+    
+    
+        container.appendChild(gridItem);
+    }
+}
 
+function resetGrid() {
+    let size = prompt('Please enter the size of the grid, max: 100');
+    if (size <= 100) {
+        const gridItems = document.querySelectorAll('.grid-element');
+        gridItems.forEach((grid) => {
+            container.removeChild(grid);
+        });
+        makeGrid(size);
+    }
+}
 
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('.reset-button');
 
-for (let i = 0; i < 16*16; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-element');
-    gridItem.addEventListener('mouseenter', toggleActive);
-
-
-
-    container.appendChild(gridItem);
-}
+resetButton.addEventListener('click', resetGrid);
+makeGrid(16);
