a980a2549fd28a13f730add099a1f0a727d04c3c
DawnBringsABloomingRose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-06-01 14:20:50 -0400

    Add function to access basic calculator operations

--- 04-calculator/script.js
+++ 04-calculator/script.js
@@ -14,3 +14,18 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(num1, num2, operand) {
+    if (operand == '+') {
+        return add(num1, num2);
+    }
+    if (operand == '-') {
+        return subtract(num1, num2);
+    }
+    if (operand == '*') {
+        return multiply(num1, num2);
+    }
+    if (operand == '/') {
+        return divide(num1, num2);
+    }
+}
+
