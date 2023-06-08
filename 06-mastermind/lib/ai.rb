75106731ab43127bd5c6586dee6611950c3cbf5f
DawnBringsABloomingARose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-06-07 20:46:39 -0400

    Add the ability to have the Ai eliminate all bad guesses based on feedback

--- 06-mastermind/lib/ai.rb
+++ 06-mastermind/lib/ai.rb
@@ -1,5 +1,11 @@
 class Ai 
   COLOURS = 'yrgbpl'.split('')
+  attr_accessor :candidate_guesses
+
+  def initialize
+    @potential_guesses = self.class.all_possible_guesses
+    @candidate_guesses = @potential_guesses
+  end
 
   def make_code
     code = []
@@ -7,5 +13,55 @@ class Ai
       code.push(COLOURS[rand(6)])
     end
     code
+  end
+
+  def first_guess
+    guess = []
+    2.times do
+      current_color = COLOURS[rand(6)]
+      2.times { guess.push(current_color) }
+    end
+    guess
+  end
+
+  def code_check(guess, code)
+    exact_right = 0
+    half_right = 0
+    guess.each_with_index do |i, peg|
+      exact_right += 1 if i == code[peg]
+    end
+    guess.uniq.each do |i|
+      half_right += [guess.count(i), code.count(i)].min
+    end
+    [exact_right, half_right - exact_right]
+  end
+
+  def eliminate_guesses(guess, feedback)
+    @candidate_guesses.each_with_index do |cg, index|
+      if code_check(guess, cg) == feedback
+        next
+      end
+      @candidate_guesses[index] = nil
+    end
+    @candidate_guesses.compact!
+    @candidate_guesses
+  end
+
+  def next_guess
+    
+  end
+
+  def self.all_possible_guesses
+    potential_guesses = []
+    COLOURS.each do |i1|
+      COLOURS.each do |i2|
+        COLOURS.each do |i3|
+          COLOURS.each do |i4|
+            potential_guesses.push([i1, i2, i3, i4])
+          end
+        end
+      end
+    end
+    potential_guesses
   end
 end
