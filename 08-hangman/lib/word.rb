e0705210babb68ec6b1985eea20bd9ae76fbb5dd
DawnBringsABloomingARose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-06-10 14:41:28 -0400

    Add ability to guess a letter

--- 08-hangman/lib/word.rb
+++ 08-hangman/lib/word.rb
@@ -1,8 +1,9 @@
 class Word
+  attr_reader :guess, :word
 
   def initialize
-    @word = self.get_random_word
-    @guess = ''
+    @word = self.class.get_random_word
+    @guess = Array.new(@word.length, '_')
   end
 
   def self.get_random_word
@@ -11,5 +12,11 @@ class Word
       viable_words.push(word) if word.strip.length >= 5 && word.length <= 12
     end
     viable_words.sample.strip
+  end
+
+  def take_guess(char)
+    @word.split('').each_with_index do |wordchar, i|
+      guess[i] = char if wordchar == char
+    end
   end
 end
