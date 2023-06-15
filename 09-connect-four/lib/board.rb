f71f482b12524769b5dcbdfd6a4a9ad41990715e
DawnBringsABloomingARose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-06-15 12:35:52 -0400

    Add the ability to check if there is a connect 4 win based on the board state

---
+++ 09-connect-four/lib/board.rb
@@ -0,0 +1,31 @@
+class Board
+  attr_accessor :pieces
+  def initialize
+    @pieces = Array.new(7, Array.new())
+  end
+
+  def check_win
+    @pieces.each_with_index do |column, index|
+      column.each_with_index do |piece, row_ind|
+        return piece if check_diagonal(index, row_ind) || check_hor(index, row_ind) || check_vert(index, row_ind)
+      end
+    end
+    false
+  end
+
+  def check_vert(col, row)
+    return true if !(@pieces[col][row].nil?) && @pieces[col][row] == @pieces[col][row + 1] && @pieces[col][row] == @pieces[col][row + 2] && @pieces[col][row] == @pieces[col][row + 3]
+    false
+  end
+
+  def check_hor(col, row)
+    return true if !(@pieces[col][row].nil?) && @pieces[col][row] == @pieces[col + 1][row] && @pieces[col][row] == @pieces[col + 2][row] && @pieces[col][row] == @pieces[col + 3][row]
+    false
+  end
+
+  def check_diagonal(col, row)
+    return true if !(@pieces[col][row].nil?) && @pieces[col][row] == @pieces[col + 1][row + 1] && @pieces[col][row] == @pieces[col + 2][row + 2] && @pieces[col][row] == @pieces[col + 3][row + 3]
+    return true if !(@pieces[col][row].nil?) && @pieces[col][row] == @pieces[col - 1][row + 1] && @pieces[col][row] == @pieces[col - 2][row + 2] && @pieces[col][row] == @pieces[col - 3][row + 3]
+    false
+  end
+end
--- 09-connect-four/spec/board_spec.rb
+++ 09-connect-four/spec/board_spec.rb
@@ -6,5 +6,29 @@ describe Board do
       board = Board.new
       expect(board.check_win).to eql(false)
     end
+
+    it "returns the winner's colour if they win on a horizontal" do
+      board = Board.new
+      4.times {|i| board.pieces[i][0] = 'b'}
+      expect(board.check_win).to eql('b')
+    end
+
+    it "returns the winner's colour if they win on a vertical" do
+      board = Board.new
+      4.times {|i| board.pieces[0][i] = 'b'}
+      expect(board.check_win).to eql('b')
+    end
+
+    it "returns the winner's colour if they win on a diagonal" do
+      board = Board.new
+      4.times {|i| board.pieces[i][i] = 'b'}
+      expect(board.check_win).to eql('b')
+    end
+
+    it "returns the winner's colour if they win on a diagonal in the other direction" do
+      board = Board.new
+      4.times {|i| board.pieces[4-i][i] = 'b'}
+      expect(board.check_win).to eql('b')
+    end
   end
 end
